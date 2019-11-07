8ad2917a2c186a23cedc6ed909187dda4475d1b3
Your Name <you@example.com>
2019-11-07 17:33:36 +0200

    first commit

--- src/main/java/com/garage/Controllers/LoginController.java
+++ src/main/java/com/garage/Controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.garage.Controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,11 @@ import com.garage.Rest.ex.InvalidLoginException;
 /**
  *  Created by raz samari on 03.11.2019.
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api")
 public class LoginController {
-    @PostMapping("/admin")
+    @PostMapping("/login")
     ResponseEntity<Boolean> login(@RequestParam String name,@RequestParam String password) throws InvalidLoginException {
         if(name.equals("admin") && password.equals("1234")){
             return ResponseEntity.ok(true);
